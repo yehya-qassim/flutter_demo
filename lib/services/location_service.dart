61a57b8b995371db768c15fae2267aabe13fd703
Yehya <yehya@finfirst.com>
2021-10-01 12:48:54 +0300

    location after asking enabling

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -16,39 +16,47 @@ class LocationService {
   }
 
   //A function to check for permission
-  Future<void> _checkPermission() async {
-    await _checkService();
+  Future<bool> _checkPermission() async {
+     if(await _checkService()){
+       //check if permission is granted
+       _permissionGranted = await _location.hasPermission();
+       if (_permissionGranted == PermissionStatus.denied) {
+         //if not request permission
+         _permissionGranted = await _location.requestPermission();
+         if (_permissionGranted != PermissionStatus.granted) {
+         }
 
-    //check if permission is granted
-    _permissionGranted = await _location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      //if not request permission
-      _permissionGranted = await _location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return;
-      }
+     }
     }
+     return _permissionGranted == PermissionStatus.granted;
   }
 
   //A function to get the location data
   Future<geo_ocation.Placemark?> getLocationData() async {
     //first check permission
-    await _checkPermission();
+    final bool permission = await _checkPermission();
+    if(permission){
+      //return the location data
+      final LocationData? locationData = await _location.getLocation();
 
-    //return the location data
-    final LocationData? locationData = await _location.getLocation();
+      //getting place mark such as country name
+      List<geo_ocation.Placemark> placeMarks = await geo_ocation.placemarkFromCoordinates(
+          locationData!.latitude!, locationData.longitude!);
+      final place = placeMarks[0];
+      return place;
+    }
 
-    //getting place mark such as country name
-    List<geo_ocation.Placemark> placeMarks = await geo_ocation.placemarkFromCoordinates(
-        locationData!.latitude!, locationData.longitude!);
-    final place = placeMarks[0];
-    return place;
+    return null;
   }
 
   //checking if service is enabled
-  _checkService() async {
+  Future<bool> _checkService() async {
     try {
       _serviceEnabled = await _location.serviceEnabled();
+      if(!_serviceEnabled!){
+        _serviceEnabled = await _location.requestService();
+      }
+
     } on PlatformException catch (err) {
       //on some android devices the check may take a little longer so we should wait till this function works without exception
       _serviceEnabled = false;
@@ -56,6 +64,8 @@ class LocationService {
       // location service is still not created
       await _checkService(); // re-invoke itself every time the error is catch, so until the location service setup is complete
     }
+
+    return _serviceEnabled!;
   }
 
 
